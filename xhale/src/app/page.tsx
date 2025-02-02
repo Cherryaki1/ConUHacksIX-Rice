c5dab85b6c0d2b654c4db19a09b3c5e4ebc3af46
Cherryaki <Botao5339@gmail.com>
2025-02-02 02:52:41 -0500

    Styling

--- xhale/src/app/page.tsx
+++ xhale/src/app/page.tsx
@@ -28,10 +28,12 @@ export default function Home() {
   //   }
   // }, [showChart]);
 
-  const tweetList = [{'tweet': 'Philly Plane Crash Crater\n\nYou Decide..... Plane or Missile ....\n\n#planecrash #phillyplanecrash', 'username': 'Culture War', 'timestamp': '2025-02-01T15:42:52.000Z'}, {'tweet': 'Bring back saying grace in public', 'username': 'The War on Beauty', 'timestamp': '2025-01-30T17:23:36.000Z'}];
+  
 
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-black">
+      
+      {/* Background Video */}
       <video
         autoPlay
         loop
@@ -42,15 +44,16 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
 
-      {/* Add your page content here */}
+      {/* Page content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full space-y-2 transition-all duration-500">
+
         {/* Search + Analyze */}
-        <div>
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 flex items-center space-x-2 p-4 mt-4">
           {/* Search Box */}
-          <div className="relative my-4 transition-all duration-500">
+          <div className="relative my-4 transition-all">
             <input
               type="text"
-              className="block w-[40em] py-2 px-0 test-sm text-white bg-stone-200 border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:text-white focus:border-emerald-600 peer rounded-lg bg-opacity-25"
+              className="block w-[40em] py-2 px-0 pl-2 text-sm text-white bg-stone-200 border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:text-white focus:border-emerald-600 peer rounded-l-lg bg-opacity-25"
               placeholder=""
             />
             <label
@@ -61,7 +64,7 @@ export default function Home() {
             </label>
           </div>{" "}
           {/* End Search Box */}
-          <button className="p-3 rounded-3xl bg-emerald-600 text-white hover:bg-white hover:text-emerald-600 duration-300">
+          <button className="px-3 border-emerald-600 h-9 rounded-r-lg bg-emerald-600 text-white hover:bg-white hover:text-emerald-600 duration-300">
             Analyze
           </button>
         </div>
